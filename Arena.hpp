e32c724cdd9ee5d03770dbe419109ece47c44dd3
Therdel <14974231+Therdel@users.noreply.github.com>
2020-02-14 01:34:58 +0100

    Split into Arenas and Allocator

---
+++ Arena.hpp
@@ -0,0 +1,49 @@
+#pragma once
+#include <memory>
+
+class Arena {
+private:
+	friend class ArenaAllocator;
+
+	// pointer to beginning of the arena storage
+	virtual auto _getStorage()-> uint8_t* = 0;
+
+	// total size of the arena
+	virtual auto _getSize()-> size_t = 0;
+
+	// pointer to unallocated/free half of arena
+	virtual auto _getWaterline()-> uint8_t*& = 0;
+};
+
+template <size_t size, size_t alignment = alignof(std::max_align_t)>
+class StackArena final : public Arena {
+public:
+	StackArena()
+	: _storage{}
+	, _waterline{ _storage } {}
+
+private:
+	alignas(alignment) uint8_t _storage[size];
+	uint8_t* _waterline;
+
+	auto _getStorage() -> uint8_t* override { return _storage; }
+	auto _getSize() -> size_t override { return size; }
+	auto _getWaterline() -> uint8_t*& override { return _waterline; }
+};
+
+class HeapArena final : public Arena {
+public:
+	HeapArena(size_t size)
+	: _storage(std::make_unique<uint8_t[]>(size))
+	, _size(size)
+	, _waterline(_storage.get()) {}
+
+private:
+	std::unique_ptr<uint8_t[]> _storage;
+	size_t _size;
+	uint8_t* _waterline;
+
+	auto _getStorage() -> uint8_t* override { return _storage.get(); }
+	auto _getSize() -> size_t override { return _size; }
+	auto _getWaterline() -> uint8_t*& override { return _waterline; }
+};
--- ArenaAllocator.hpp
+++ ArenaAllocator.hpp
@@ -1,89 +1,42 @@
 #pragma once
+#include "Arena.hpp"
 
-#include <memory>
-#include <vector>
-
-template <size_t size, size_t alignment = alignof(std::max_align_t)>
-class Arena {
+class ArenaAllocator {
 public:
-	Arena();
+	ArenaAllocator(Arena &arena)
+	: _arena(arena) {}
 
+	// returns a pointer to storage aligned and sized for n elements of type T
 	template<typename T>
-	auto allocate() -> void*;
+	auto allocate(size_t n = 1) -> void*;
 
+	// allocates and in-place constructs a T object and returns a pointer to it
 	template<typename T, typename ...Args>
 	auto construct(Args&& ...args)->T*;
 
 private:
-	alignas(alignment) uint8_t _storage[size];
-	uint8_t* _waterline;
-};
-
-template<size_t size, size_t alignment>
-Arena<size, alignment>::Arena()
-	: _storage{}
-	, _waterline{ &_storage[0] } {
-}
-
-template<size_t size, size_t alignment>
-template<typename T>
-auto Arena<size, alignment>::allocate() -> void* {
-	uint8_t* pastTheEnd = _storage + size;
-	size_t capacity = pastTheEnd - _waterline;
-	auto &waterlineRef = reinterpret_cast<void*&>(_waterline);
-	void* storage = std::align(alignof(T), sizeof(T), waterlineRef, capacity);
-	if (storage != nullptr) {
-		_waterline = reinterpret_cast<uint8_t*>(storage) + sizeof(T);
-		return storage;
-	}
-	return nullptr;
-}
-
-template<size_t size, size_t alignment>
-template<typename T, typename ...Args>
-auto Arena<size, alignment>::construct(Args&&... args)->T* {
-	void* storage = allocate<T>();
-	if (!storage) { return nullptr; }
-
-	return new(storage) T(std::forward<Args>(args)...);
-}
-
-class HeapArena {
-public:
-	HeapArena(size_t size)
-		: _storage(size)
-		, _waterline(_storage.data()) {
-	}
-
-	template<typename T>
-	auto allocate(size_t n=1) -> void*;
-
-	template<typename T, typename ...Args>
-	auto construct(Args&& ...args)->T*;
-
-private:
-	std::vector<uint8_t> _storage;
-	uint8_t *_waterline;	
+	Arena& _arena;
 };
 
 template<typename T>
-auto HeapArena::allocate(size_t n) -> void* {
-	uint8_t* pastTheEnd = _storage.data() + _storage.size();
+auto ArenaAllocator::allocate(size_t n) -> void* {
+	uint8_t* pastTheEnd = _arena._getStorage() + _arena._getSize();
 
-	size_t storage_size = n * sizeof(T);
-	auto& waterlineRef = reinterpret_cast<void*&>(_waterline);
-	size_t capacity = pastTheEnd - _waterline;
+	size_t requested_size = n * sizeof(T);
+	auto& waterlineRef = _arena._getWaterline();
+	auto& waterlineRawRef = reinterpret_cast<void*&>(waterlineRef);
+	size_t capacity = pastTheEnd - waterlineRef;
 
-	void* storage = std::align(alignof(T), storage_size, waterlineRef, capacity);
+	void* storage = std::align(alignof(T), requested_size, waterlineRawRef, capacity);
 	if (storage != nullptr) {
-		_waterline = reinterpret_cast<uint8_t*>(storage) + storage_size;
+		waterlineRef = reinterpret_cast<uint8_t*>(storage) + requested_size;
 		return storage;
 	}
 	return nullptr;
 }
 
 template<typename T, typename ...Args>
-auto HeapArena::construct(Args&&... args)->T* {
+auto ArenaAllocator::construct(Args&& ...args) -> T* {
 	void* storage = allocate<T>();
 	if (!storage) { return nullptr; }
 
